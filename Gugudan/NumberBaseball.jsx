384d0bd906e3413fead523cbc34d3146385500d8
Chuhj <chj9574@naver.com>
2021-02-24 23:12:18 +0900

    3-8

--- Gugudan/NumberBaseball.jsx
+++ Gugudan/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Try from './Try';
 
 function getNumbers() { // 숫자 4개 랜덤으로 뽑음
@@ -6,91 +6,76 @@ function getNumbers() { // 숫자 4개 랜덤으로 뽑음
   const array = [];
   for (let i = 0; i < 4; i += 1) {
     const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
-    console.log(chosen);
     array.push(chosen);
   }
   return array;
 }
 
-class NumberBaseball extends Component {
-  state = {
-    result: '',
-    value: '',
-    answer: getNumbers(),
-    tries: [],
-  };
+const NumberBaseball = () => {
+  const [result, setResult] = useState('');
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
 
-  onSubmitForm = (e) => {
+  const onSubmitForm = (e) => {
     e.preventDefault();
-    if (this.state.value === this.state.answer.join('')) {
-      this.setState({
-        result: '홈런',
-        tries: [...this.state.tries, { try: this.state.value, result: '홈런' }],
+    if (value === answer.join('')) {
+      setResult('홈런');
+      setTries((prevTries) => {
+        return [...prevTries, { try: value, result: '홈런' }]
       });
       alert('게임을 다시 시작합니다.');
-      this.setState({
-        value: '',
-        answer: getNumbers(),
-        tries: [],
-      });
+      setValue('');
+      setAnswer(getNumbers());
+      setTries([]);
     }
     else {
-      const answerArray = this.state.value.split('').map((v) => parseInt(v));
+      const answerArray = value.split('').map((v) => parseInt(v));
       let strike = 0;
       let ball = 0;
-      if (this.state.tries.length >= 9) {
-        this.setState({
-          result: `실패! 답은 ${this.state.answer.join(',')}였습니다.`,
-        });
+      if (tries.length >= 9) {
+        setResult(`실패! 답은 ${answer.join(',')}였습니다.`);
         alert('게임을 다시 시작합니다.');
-        this.setState({
-          value: '',
-          answer: getNumbers(),
-          tries: [],
-        });
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
+        setResult('');
       }
       else {
         for (let i = 0; i < 4; i += 1) {
-          if (answerArray[i] === this.state.answer[i]) {
+          if (answerArray[i] === answer[i]) {
             strike += 1;
           }
-          else if (this.state.answer.includes(answerArray[i])) {
+          else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
         }
-        this.setState({
-          tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크 ${ball} 볼`}],
-          value: '',
-        })
+        setTries((prevTries) => { return [...prevTries, {try: value, result: `${strike} 스트라이크 ${ball} 볼`}]});
+        setValue('');
       }
     }
   };
 
-  onChangeInput = (e) => {
-    console.log(this.state.answer);
-    this.setState({
-      value: e.target.value,
-    });
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   };
 
-  render() {
-    return (
-      <>
-        <h1>{this.state.result}</h1>
-        <form onSubmit={this.onSubmitForm}>
-          <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} type="text" />
-        </form>
-        <div>시도: {this.state.tries.length}</div>
-        <ul>
-          {this.state.tries.map((v, i) => {
-            return (
-              <Try key={i} tryInfo={v} />
-            );
-          })}
-        </ul>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input maxLength={4} value={value} onChange={onChangeInput} type="text" />
+      </form>
+      <div>시도: {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return (
+            <Try key={i} tryInfo={v} />
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default NumberBaseball;
