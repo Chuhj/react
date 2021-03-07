3adee90a1a8f6e94873cd925a097a5e89fe675e4
Chuhj <chj9574@naver.com>
2021-03-08 03:43:00 +0900

    7-4

---
+++ lecture/TicTacToe.jsx
@@ -0,0 +1,107 @@
+import React, { useCallback, useReducer, useEffect } from 'react';
+import Table from './Table';
+
+const initialState = {
+  winner: '',
+  turn: 'O',
+  tableData: [['', '', ''], ['', '', ''], ['', '', '']],
+  current: [-1, -1],
+};
+
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL';
+export const CHANGE_TURN = 'CHANGE_TURN';
+export const RESET_GAME = 'RESET_GAME';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_WINNER:
+      return {
+        ...state,
+        winner: action.winner,
+      };
+    case CLICK_CELL:{
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...tableData[action.row]]; // immer 라이브러리로 가독성 해결
+      tableData[action.row][action.cell] = state.turn;
+      return {
+        ...state,
+        tableData: tableData,
+        current: [action.row, action.cell],
+      };
+    }
+    case CHANGE_TURN:{
+      return {
+        ...state,
+        turn: state.turn === 'O' ? 'X' : 'O',
+      };
+    }
+    case RESET_GAME:{
+      return {
+        ...state,
+        turn: 'O',
+        tableData: [['', '', ''], ['', '', ''], ['', '', '']],
+        current: [-1, -1],
+      };
+    }
+  }
+};
+
+const TicTacToe = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { winner, turn, tableData, current } = state;
+  // const [winner, setWinner] = useState('');
+  // const [turn, setTurn] = useState('O');
+  // const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+
+  const onClickTable = useCallback(() => {
+    dispatch({type: SET_WINNER, winner: 'O'});
+  }, []);
+
+  useEffect(() => { // 승자 체크
+    const [row, cell] = current;
+    if (row < 0 || cell < 0){
+      return;
+    }
+    let win = false;
+    console.log(row, cell,turn, tableData);
+    if (tableData[row][0] === turn && tableData[row][1] === turn && tableData[row][2] === turn){
+      win = true;
+    } else if (tableData[0][cell] === turn && tableData[1][cell] === turn && tableData[2][cell] === turn){
+      win = true;
+    } else if (tableData[0][0] === turn &&  tableData[1][1] === turn && tableData[2][2] === turn) {
+      win = true;
+    } else if (tableData[0][2] === turn && tableData[1][1] === turn && tableData[2][0] === turn) {
+      win = true;
+    }
+    
+    if (win) {
+      dispatch({ type: SET_WINNER, winner: turn });
+      dispatch({ type: RESET_GAME });
+    } else {
+      let all = true;
+      tableData.forEach((row) => {
+        row.forEach((cell) => {
+          if (!cell) {
+            all = false;
+          }
+        });
+      });
+      if (all) {
+        dispatch({ type: RESET_GAME });
+      }
+      else{
+        dispatch({ type: CHANGE_TURN });
+      }
+    }
+  }, [current]);
+
+  return (
+    <>
+      <Table onClick={onClickTable} tableData={tableData} dispatch={dispatch}/>
+      {winner && <div>{winner}님의 승리</div>}
+    </>
+  )
+};
+
+export default TicTacToe;
