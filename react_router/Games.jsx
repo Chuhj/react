0f9c488a3df35996d0cc9858bac2c53da280512b
Chuhj <chj9574@naver.com>
2021-03-14 14:58:13 +0900

    9-3

--- react_router/Games.jsx
+++ react_router/Games.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { BrowserRouter, HashRouter, Route, Link } from 'react-router-dom';
 import WordRelay from '../WordRelay/WordRelayClass';
 import Lotto from '../lecture/LottoClass';
+import GameMatcher from './GameMatcher'
 
 const Games = () => {
   return (
     <BrowserRouter>
       <div>
-        <Link to="/word-relay">끝말잇기</Link>
+        <Link to="/game/word-relay">끝말잇기</Link>
         &nbsp;
-        <Link to="/lotto-generator">로또</Link>
+        <Link to="/game/lotto-generator">로또</Link>
+        &nbsp;
+        <Link to="/game/index">게임매쳐</Link>
       </div>
       <div>
         <Route path="/word-relay" component={WordRelay} />
         <Route path="/lotto-generator" component={Lotto} />
+        <Route path="/game/:name" component={GameMatcher} />
       </div>
     </BrowserRouter>
   );
